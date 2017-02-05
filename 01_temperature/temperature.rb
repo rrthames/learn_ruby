82f222b4549d73d8c685f4ddfc6f0e2d069e1f03
Robert Raymond Darrell Thames <robert.r.d.thames@gmail.com>
2017-02-05 13:42:32 -0500

    3 down

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
-#write your code here
+def ftoc(temp)
+  return (temp - 32) * 5 / 9
+end
+
+def ctof(temp)
+  return (temp.to_f * 9 / 5) + 32
+end
+
+puts ctof(37)
+
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
-#write your code here
+def add(a, b)
+  return a + b
+end
+
+def subtract(a, b)
+  return a - b
+end
+
+def sum(array)
+  a = 0
+  array.each do |item|
+      a = a + item
+  end
+  return a
+end
+
+def multiply(*num)
+  a = 1
+  num.each do |item|
+      a = a * item
+  end
+  return a
+end
+
+def power(num, pow)
+  return num ** pow
+end
+
+def factorial(num)
+  
+  return num == 0 ? 1 : num.downto(1).inject(:*)
+end
